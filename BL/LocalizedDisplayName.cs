4925d0bf9ea46d494ea3b989bb3242943445a3e2
Selitsckiy Dmitriy <dmx333dmx@mail.ru>
2015-03-27 17:14:23 +0200

    Koment

--- BL/LocalizedDisplayName.cs
+++ BL/LocalizedDisplayName.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+//afsafp;asl,gkaoga;lgwearohbkrkthotrmthmp;htrskhotpkotshko[k[hsk[pskhslsdf
 namespace BL
 {
     public class LocalizedDisplayNameAttribute : DisplayNameAttribute
